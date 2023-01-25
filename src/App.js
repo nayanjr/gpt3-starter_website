a084af0554113fef81c456f66ef3f2e7850f9d83
nayanjr <kamalnayan.edu2020@gmail.com>
2023-01-25 15:55:26 -0600

    Starter react web application using figma design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import Article from './components/article/Article';
+import Brand from './components/brand/Brand';
+import Cta from './components/cta/Cta';
+import Feature from './components/feature/Feature';
+import Navbar from './components//navbar/Navbar';
+import Blog from './containers/blog/Blog';
+import Features from './containers/features/Features';
+import Footer from './containers/footer/Footer';
+import Header from './containers/header/Header';
+import Possibility from './containers/possibility/Possibility';
+import WHATGPT3 from './containers/whatGPT3/WHATGPT3';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <div className="App">
+            <div className="gradient__bg">
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WHATGPT3 />
+            <Features />
+            <Possibility />
+            <Cta />
+            <Blog />
+            <Footer />
+        </div>
+    )
 }
-
-export default App;
